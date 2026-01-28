5c29d6a923614c367837a6f8781d812d3a298bfa
kumankoishi.py <7iggif@naver.com>
2026-01-28 20:07:18 +0900

    refactor: select main rather than .main (class) in app.module.css

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,13 +2,13 @@ import { Title } from '^/shared/ui/title/title';
 import { Board } from '^/widgets/board/board';
 import { ControlPanel } from '^/widgets/control-panel/control-panel';
 import { GameOverOverlay } from '^/widgets/game-over-overlay/game-over-overlay';
-import styles from './app.module.css';
+import './app.module.css';
 import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <main className={styles.main}>
+      <main>
         <Title />
         <Board />
         <ControlPanel />
