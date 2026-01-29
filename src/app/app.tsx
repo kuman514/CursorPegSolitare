0f0c7cb7a1f1d854a956aeec528bb2b66fd8271c
kumankoishi.py <7iggif@naver.com>
2026-01-29 13:19:40 +0900

    refactor: move main styling to global.css since main is unique in app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,7 +3,6 @@ import { Board } from '^/widgets/board/board';
 import { ControlPanel } from '^/widgets/control-panel/control-panel';
 import { GameOverOverlay } from '^/widgets/game-over-overlay/game-over-overlay';
 
-import './app.module.css';
 import './styles/global.css';
 
 export function App() {
